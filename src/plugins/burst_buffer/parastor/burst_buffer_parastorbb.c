308bb953c640e3b04f00b6886833fc1a60d33a3a
qinyh1 <qinyh@sugon.com>
2026-01-20 09:46:56 +0800

    fix(bb): comment out unused functions

--- src/plugins/burst_buffer/parastor/burst_buffer_parastorbb.c
+++ src/plugins/burst_buffer/parastor/burst_buffer_parastorbb.c
@@ -232,7 +232,7 @@ static void _queue_teardown(bb_job_t *bb_job, bool *clean_finish);
 static int _bb_get_parastors_state(void);
 static void *_start_stage_out(void *x);
 // static void *_start_teardown(void *x);
-static int _calibrate_task_state(uint32_t job_id, int *bb_task_ids, int index_tasks, bb_minimal_config_t *bb_min_config);
+// static int _calibrate_task_state(uint32_t job_id, int *bb_task_ids, int index_tasks, bb_minimal_config_t *bb_min_config);
 static bb_minimal_config_t *_create_bb_min_config(bb_config_t *bb_config);
 static void _bb_min_config_free(bb_minimal_config_t * config);
 static void *_cleanup_bb_resources_from_alloc(void *x);
@@ -2409,33 +2409,33 @@ extern time_t bb_p_job_get_est_start(job_record_t *job_ptr)
  * bb_state.bb_mutex is locked on function entry.
  * job_ptr may be NULL if not found
  */
-static void _purge_bb_files(uint32_t job_id, job_record_t *job_ptr)
-{
-	char *hash_dir = NULL, *job_dir = NULL;
-	char *script_file = NULL, *path_file = NULL;
-	int hash_inx;
+// static void _purge_bb_files(uint32_t job_id, job_record_t *job_ptr)
+// {
+// 	char *hash_dir = NULL, *job_dir = NULL;
+// 	char *script_file = NULL, *path_file = NULL;
+// 	int hash_inx;
 
-	hash_inx = job_id % 10;
-	xstrfmtcat(hash_dir, "%s/hash.%d",
-		   slurm_conf.state_save_location, hash_inx);
-	(void) mkdir(hash_dir, 0700);
-	xstrfmtcat(job_dir, "%s/job.%u", hash_dir, job_id);
-	(void) mkdir(job_dir, 0700);
+// 	hash_inx = job_id % 10;
+// 	xstrfmtcat(hash_dir, "%s/hash.%d",
+// 		   slurm_conf.state_save_location, hash_inx);
+// 	(void) mkdir(hash_dir, 0700);
+// 	xstrfmtcat(job_dir, "%s/job.%u", hash_dir, job_id);
+// 	(void) mkdir(job_dir, 0700);
 
-	xstrfmtcat(path_file, "%s/pathfile", job_dir);
-	(void) unlink(path_file);
-	xfree(path_file);
+// 	xstrfmtcat(path_file, "%s/pathfile", job_dir);
+// 	(void) unlink(path_file);
+// 	xfree(path_file);
 
-	if (!job_ptr || (job_ptr->batch_flag == 0)) {
-		xstrfmtcat(script_file, "%s/script", job_dir);
-		(void) unlink(script_file);
-		xfree(script_file);
-	}
+// 	if (!job_ptr || (job_ptr->batch_flag == 0)) {
+// 		xstrfmtcat(script_file, "%s/script", job_dir);
+// 		(void) unlink(script_file);
+// 		xfree(script_file);
+// 	}
 
-	(void) unlink(job_dir);
-	xfree(job_dir);
-	xfree(hash_dir);
-}
+// 	(void) unlink(job_dir);
+// 	xfree(job_dir);
+// 	xfree(hash_dir);
+// }
 
 // static void *_start_teardown(void *x)
 // {
@@ -2767,37 +2767,37 @@ static int _alloc_job_bb(job_record_t *job_ptr, bb_job_t *bb_job,
 	return rc;
 }
 
-static int _calibrate_task_state(uint32_t job_id, int *bb_task_ids, int index_tasks, bb_minimal_config_t *bb_min_config)
-{
-	if (bb_min_config == NULL) {
-		error("failed to get bb minimal config for JobId=%u", job_id);
-		return SLURM_ERROR;
-	}
-	bb_response *resp_out = xmalloc(sizeof(bb_response));
-	for (int i = 0; i < index_tasks; i++) {
-		bb_attribute_task *bb_task = xmalloc(sizeof(bb_attribute_task)) ;
+// static int _calibrate_task_state(uint32_t job_id, int *bb_task_ids, int index_tasks, bb_minimal_config_t *bb_min_config)
+// {
+// 	if (bb_min_config == NULL) {
+// 		error("failed to get bb minimal config for JobId=%u", job_id);
+// 		return SLURM_ERROR;
+// 	}
+// 	bb_response *resp_out = xmalloc(sizeof(bb_response));
+// 	for (int i = 0; i < index_tasks; i++) {
+// 		bb_attribute_task *bb_task = xmalloc(sizeof(bb_attribute_task)) ;
 
-		int rc = get_single_burst_buffer_tasks(bb_task_ids[i], bb_task, bb_min_config, resp_out);
-		if (rc != 0) {
-			error("BB-----can't get task %d", bb_task_ids[i]);
-			_bb_min_config_free(bb_min_config);
-			free_bb_response(resp_out);
-			return SLURM_ERROR;
-		}
-		/* 任务状态非完成（作业运行完时），则预热失败 */
-		debug("BB-----the state of task %d of job %u is %s", bb_task_ids[i], job_id, bb_task->task_state);
-		// TODO:预热失败信息需要进一步处理
-		if (bb_task->task_state == BB_TASK_STATE_COMPLETED) {
-			debug("BB-----the task %d of job %u haven't finish cache prefetch", bb_task_ids[i], job_id);
-			_bb_min_config_free(bb_min_config);
-			free_bb_response(resp_out);
-			return SLURM_ERROR;
-		}
-		slurm_free_task(bb_task);
-	}
-	free_bb_response(resp_out);
-	return SLURM_SUCCESS;
-}
+// 		int rc = get_single_burst_buffer_tasks(bb_task_ids[i], bb_task, bb_min_config, resp_out);
+// 		if (rc != 0) {
+// 			error("BB-----can't get task %d", bb_task_ids[i]);
+// 			_bb_min_config_free(bb_min_config);
+// 			free_bb_response(resp_out);
+// 			return SLURM_ERROR;
+// 		}
+// 		/* 任务状态非完成（作业运行完时），则预热失败 */
+// 		debug("BB-----the state of task %d of job %u is %s", bb_task_ids[i], job_id, bb_task->task_state);
+// 		// TODO:预热失败信息需要进一步处理
+// 		if (bb_task->task_state == BB_TASK_STATE_COMPLETED) {
+// 			debug("BB-----the task %d of job %u haven't finish cache prefetch", bb_task_ids[i], job_id);
+// 			_bb_min_config_free(bb_min_config);
+// 			free_bb_response(resp_out);
+// 			return SLURM_ERROR;
+// 		}
+// 		free_bb_task(bb_task);
+// 	}
+// 	free_bb_response(resp_out);
+// 	return SLURM_SUCCESS;
+// }
 
 /*
  * Attempt to allocate resources and begin file staging for pending jobs.
