098c97ce6d4e91817b0cdf11b7c9d2c9c9b6ef23
wangzh <wangzh5@sugon.com>
2026-01-27 18:31:25 +0800

    Fix avoid hardcoding

--- src/interfaces/burst_buffer.c
+++ src/interfaces/burst_buffer.c
@@ -117,7 +117,7 @@ static const char *syms[] = {
 	"bb_p_job_test_stage_out",
 	"bb_p_job_cancel",
 	"bb_p_run_script",
-	"bb_p_xlate_bb_2_tres_str"
+	"bb_p_xlate_bb_2_tres_str",
 #ifdef __METASTACK_OPT_SCHE_CHECK_BBQUOTA
 	"bb_p_get_node_quota"
 #endif
